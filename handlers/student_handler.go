763d0d8e883f4e6d7f8f3a61c95b02c071eeb183
Suttipot Prathoumthong - 6609650699 <144650601+SuttipotPrathoumthong@users.noreply.github.com>
2026-02-23 20:37:30 +0700

    Add update and delete function

--- handlers/student_handler.go
+++ handlers/student_handler.go
@@ -46,3 +46,30 @@ func (h *StudentHandler) CreateStudent(c *gin.Context) {
 
 	c.JSON(http.StatusCreated, student)
 }
+
+func (h *StudentHandler) UpdateStudent(c *gin.Context) {
+    id := c.Param("id")
+    var student models.Student
+    
+    if err := c.ShouldBindJSON(&student); err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input: " + err.Error()})
+        return
+    }
+
+    if err := h.Service.UpdateStudent(id, student); err != nil {
+        c.JSON(http.StatusNotFound, gin.H{"error": "Student not found"})
+        return
+    }
+    
+    student.Id = id
+    c.JSON(http.StatusOK, student)
+}
+
+func (h *StudentHandler) DeleteStudent(c *gin.Context) {
+    id := c.Param("id")
+    if err := h.Service.DeleteStudent(id); err != nil {
+        c.JSON(http.StatusNotFound, gin.H{"error": "Student not found"})
+        return
+    }
+    c.JSON(http.StatusNoContent, nil)
+}
