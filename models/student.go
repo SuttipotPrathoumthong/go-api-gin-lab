4f2e3dc6fba6c46e529351cb497652676f4e10f2
Suttipot Prathoumthong - 6609650699 <144650601+SuttipotPrathoumthong@users.noreply.github.com>
2026-02-23 20:33:44 +0700

    Update model for validation

--- models/student.go
+++ models/student.go
@@ -1,8 +1,8 @@
 package models
 
 type Student struct {
-	Id    string  `json:"id"`
-	Name  string  `json:"name"`
-	Major string  `json:"major"`
-	GPA   float64 `json:"gpa"`
+	Id    string  `json:"id" binding:"required"`
+    Name  string  `json:"name" binding:"required"`
+    Major string  `json:"major" binding:"required"`
+    GPA   float64 `json:"gpa" binding:"required,min=0,max=4"`
 }
