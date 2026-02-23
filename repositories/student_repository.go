c7ac40551a87b1e3a9cd20341b508557dc03f2b3
Suttipot Prathoumthong - 6609650699 <144650601+SuttipotPrathoumthong@users.noreply.github.com>
2026-02-23 20:34:19 +0700

    commit function update

--- repositories/student_repository.go
+++ repositories/student_repository.go
@@ -47,3 +47,19 @@ func (r *StudentRepository) Create(s models.Student) error {
 	)
 	return err
 }
+
+func (r *StudentRepository) Update(id string, s models.Student) error {
+    result, err := r.DB.Exec(
+        "UPDATE students SET name = ?, major = ?, gpa = ? WHERE id = ?",
+        s.Name, s.Major, s.GPA, id,
+    )
+    if err != nil {
+        return err
+    }
+    
+    rows, _ := result.RowsAffected()
+    if rows == 0 {
+        return sql.ErrNoRows
+    }
+    return nil
+}
